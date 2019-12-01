9bd040160a34e444573bd39aca736a2bb8f865d8
James Sholdice <james.sholdice@gmail.com>
2019-11-30 20:23:07 -0500

    moved global game object to class

--- Adway_Alpha/coreData.js
+++ Adway_Alpha/coreData.js
@@ -10,15 +10,15 @@
 // --Class/job mastery points
 
 class Achievement {
-    constructor(listenerType, handlerFunc){
+    constructor(listenerType, handlerFunc) {
 
         this.handlerFunc = handlerFunc;
         this.HandlerID = allEvents.registerListener(listenerType, handlerFunc);
     }
 }
 
-class TieredAchievement extends Achievement{
-    constructor(rewardBreakpoints, rewardValues,handlerType, handlerFunc){
+class TieredAchievement extends Achievement {
+    constructor(rewardBreakpoints, rewardValues, handlerType, handlerFunc) {
         super(handlerType, handlerFunc);
 
         this.TierBreakpoints = rewardBreakpoints;
@@ -28,247 +28,227 @@ class TieredAchievement extends Achievement{
     }
 }
 
-class Actor{}
+class Actor { }
 
-class Hero extends Actor{}
+class Hero extends Actor { }
 
-class GameClass{
+class GameStore {
+
     constructor() {
-        this = {
-            Resources: {
-                Scraps: 0,
-                ScrapsIncome: 1,
-                ScrapConversionRate: 0,
-                ScrapConversionEfficiency: 0.5,
 
-                ScrapToMetal: 0,
-                ScrapToLeather: 0,
-                ScrapToCloth: 0,
-                Metal: 0,
-                Leather: 0,
-                Cloth: 0,
-            }
-        }
-    }
-}
+        // Resources
+        this.Resources = {
+            Scraps: 0,
+            ScrapsIncome: 1,
+            ScrapConversionRate: 0,
+            ScrapConversionEfficiency: 0.5,
 
-var Game = {
-
-    // Resources
-    Resources: {
-        Scraps: 0,
-        ScrapsIncome: 1,
-        ScrapConversionRate: 0,
-        ScrapConversionEfficiency: 0.5,
-
-        ScrapToMetal: 0,
-        ScrapToLeather: 0,
-        ScrapToCloth: 0,
-        Metal: 0,
-        Leather: 0,
-        Cloth:  0,
-    },
-
-    // Alternative, testing
-    Heroes: [
-        {
-            Name: "Meryl",
-            Level: 1,
-            Speed: 15,
-            Attack: 10,
-            HealthCurr: 100,
-            HealthMax: 100,
-            isAlive: true,
-            CurrentTurnOrder: 0,
-
-            SpeedBase: 15,
-            AttackBase: 10,
-            HealthBase: 100,
+            ScrapToMetal: 0,
+            ScrapToLeather: 0,
+            ScrapToCloth: 0,
+            Metal: 0,
+            Leather: 0,
+            Cloth: 0,
         },
-        {
-            Name: "Chase",
-            Level: 1,
-            Speed: 14,
-            Attack: 10,
-            HealthMax: 100,
-            HealthCurr: 100,
-            isAlive: true,
-            CurrentTurnOrder: 0,
 
-            SpeedBase: 14,
-            AttackBase: 10,
-            HealthBase: 100,
-        },
-        {
-            Name: "Tali",
-            Level: 1,
-            Speed: 16,
-            Attack: 10,
-            HealthMax: 100,
-            HealthCurr: 100,
-            isAlive: true,
-            CurrentTurnOrder: 0,
+            // Alternative, testing
+            this.Heroes = [
+                {
+                    Name: "Meryl",
+                    Level: 1,
+                    Speed: 15,
+                    Attack: 10,
+                    HealthCurr: 100,
+                    HealthMax: 100,
+                    isAlive: true,
+                    CurrentTurnOrder: 0,
 
-            SpeedBase: 16,
-            AttackBase: 10,
-            HealthBase: 100,
-        },
-        {
-            Name: "Herschel",
-            Level: 1,
-            Speed: 15,
-            Attack: 10,
-            HealthMax: 100,
-            HealthCurr: 100,
-            isAlive: true,
-            CurrentTurnOrder: 0,
+                    SpeedBase: 15,
+                    AttackBase: 10,
+                    HealthBase: 100,
+                },
+                {
+                    Name: "Chase",
+                    Level: 1,
+                    Speed: 14,
+                    Attack: 10,
+                    HealthMax: 100,
+                    HealthCurr: 100,
+                    isAlive: true,
+                    CurrentTurnOrder: 0,
 
-            SpeedBase: 15,
-            AttackBase: 10,
-            HealthBase: 100,
-        },
-    ],
+                    SpeedBase: 14,
+                    AttackBase: 10,
+                    HealthBase: 100,
+                },
+                {
+                    Name: "Tali",
+                    Level: 1,
+                    Speed: 16,
+                    Attack: 10,
+                    HealthMax: 100,
+                    HealthCurr: 100,
+                    isAlive: true,
+                    CurrentTurnOrder: 0,
 
-    EnemyTemplates: [
-        {
-            Name: "Goblin",
-            AttackMod: 1,
-            HealthMod: 1,
-            SpeedMod: 1,
-        },
-        {
-            Name: "Dragon",
-            AttackMod: 2,
-            HealthMod: 5,
-            SpeedMod: 1.2,
-        }
-    ],
+                    SpeedBase: 16,
+                    AttackBase: 10,
+                    HealthBase: 100,
+                },
+                {
+                    Name: "Herschel",
+                    Level: 1,
+                    Speed: 15,
+                    Attack: 10,
+                    HealthMax: 100,
+                    HealthCurr: 100,
+                    isAlive: true,
+                    CurrentTurnOrder: 0,
 
-    Enemies: [],
+                    SpeedBase: 15,
+                    AttackBase: 10,
+                    HealthBase: 100,
+                },
+            ],
 
-    // Current Map/World info
-    World: {
-        CurrentZone: 0,
-        CurrentCell: 0,
-        WorldZoneScaleFactor: 2, // Enemy Multi-per zone
-        WorldCellScaleFactor: 0.021, // Enemy additive per cell
-    },
+            this.EnemyTemplates = [
+                {
+                    Name: "Goblin",
+                    AttackMod: 1,
+                    HealthMod: 1,
+                    SpeedMod: 1,
+                },
+                {
+                    Name: "Dragon",
+                    AttackMod: 2,
+                    HealthMod: 5,
+                    SpeedMod: 1.2,
+                }
+            ],
 
-    // Per run values
+            this.Enemies = [],
 
-    // Persistent values
-    Persistents: {
-        // Achievements
-        Achievements: {
-            TotalScore: 0,
-
-            // Scrap Collections
-            Scraps: {
-                BreakpointEarned: 0,
-                HandlerID: 0,
-                TierBreakpoints: [10, 50, 100, 1000, 10000],
-                TierValues: [1, 1, 2, 2, 5],
-                AchievementHandler: 
-                    function () {
-
-                        let base = Game.Persistents.Achievements.Scraps;
-                        
-                        let nextTier = base.TierBreakpoints[base.BreakpointEarned];
-
-                        if (Game.Resources.Scraps >= nextTier) {
-                            console.log(ParseGameText("Achievement recieved: Acquire {0} Scraps!", nextTier));
-
-                            Game.Persistents.Achievements.TotalScore += base.TierValues[base.BreakpointEarned++];
-                        }
-
-                        if (base.BreakpointEarned >= base.TierBreakpoints.length) {
-                            allEvents.removeEvent(base.HandlerID);
-                        }
-                    },
+            // Current Map/World info
+            this.World = {
+                CurrentZone: 0,
+                CurrentCell: 0,
+                WorldZoneScaleFactor: 2, // Enemy Multi-per zone
+                WorldCellScaleFactor: 0.021, // Enemy additive per cell
             },
 
-            LargestSingle: 
-            {
-                HandlerID: 0,
-                BreakpointEarned: 0,
-                ActualLargest: 0,
-                TierBreakpoints: [10, 50, 100, 1000, 10000],
-                TierValues: [1, 2, 5, 5, 15],
-                AchievementHandler: 
-                    function (source, dest, hitSize) {
-                        let base = Game.Persistents.Achievements.LargestSingle;
+            // Persistent values
+            this.Persistents = {
+                // Achievements
+                Achievements: {
+                    TotalScore: 0,
 
-                        if (hitSize > base.ActualLargest) {
-                            base.ActualLargest = hitSize;
-                            if (base.ActualLargest > base.TierBreakpoints[base.BreakpointEarned]) {
-                                console.log(ParseGameText('Achievement acquired: Largest single hit {0} or greater',
-                                formatNumber(base.TierBreakpoints[base.BreakpointEarned])));
+                    // Scrap Collections
+                    Scraps: {
+                        BreakpointEarned: 0,
+                        HandlerID: 0,
+                        TierBreakpoints: [10, 50, 100, 1000, 10000],
+                        TierValues: [1, 1, 2, 2, 5],
+                        AchievementHandler:
+                            function () {
+
+                                let base = Game.Persistents.Achievements.Scraps;
+
+                                let nextTier = base.TierBreakpoints[base.BreakpointEarned];
+
+                                if (Game.Resources.Scraps >= nextTier) {
+                                    console.log(ParseGameText("Achievement recieved: Acquire {0} Scraps!", nextTier));
+
+                                    Game.Persistents.Achievements.TotalScore += base.TierValues[base.BreakpointEarned++];
+                                }
+
+                                if (base.BreakpointEarned >= base.TierBreakpoints.length) {
+                                    allEvents.removeEvent(base.HandlerID);
+                                }
+                            },
+                    },
+
+                    LargestSingle:
+                    {
+                        HandlerID: 0,
+                        BreakpointEarned: 0,
+                        ActualLargest: 0,
+                        TierBreakpoints: [10, 50, 100, 1000, 10000],
+                        TierValues: [1, 2, 5, 5, 15],
+                        AchievementHandler:
+                            function (source, dest, hitSize) {
+                                let base = Game.Persistents.Achievements.LargestSingle;
+
+                                if (hitSize > base.ActualLargest) {
+                                    base.ActualLargest = hitSize;
+                                    if (base.ActualLargest > base.TierBreakpoints[base.BreakpointEarned]) {
+                                        console.log(ParseGameText('Achievement acquired: Largest single hit {0} or greater',
+                                            formatNumber(base.TierBreakpoints[base.BreakpointEarned])));
+
+                                        Game.Persistents.Achievements.TotalScore += base.TierValues[base.BreakpointEarned++];
+                                    }
+
+                                    if (base.BreakpointEarned > base.TierBreakpoints.length) {
+                                        allEvents.removeEvent(base.HandlerID);
+                                    }
+                                }
+                            },
+                    },
+
+                    Metal: new TieredAchievement(
+                        [10, 25, 50, 100, 1000],
+                        [1, 1, 2, 2, 5],
+                        allEvents.EventTypes.METAL_RECIEVED,
+                        function () {
+                            let base = Game.Persistents.Achievements.Metal;
+
+                            let nextTier = base.TierBreakpoints[base.BreakpointEarned];
+
+                            if (Game.Resources.Metal >= nextTier) {
+                                console.log(ParseGameText('Achievement Recieved: ' + GameText.English.AchievementText.Metal.Criteria, nextTier));
 
                                 Game.Persistents.Achievements.TotalScore += base.TierValues[base.BreakpointEarned++];
                             }
 
-                            if (base.BreakpointEarned > base.TierBreakpoints.length) {
+                            if (base.BreakpointEarned >= base.TierBreakpoints.length) {
                                 allEvents.removeEvent(base.HandlerID);
                             }
                         }
+
+                    )
+
+                },
+
+                Stats: {
+                    GameVersion: "NaNi",
+                    LastUpdateTime: 0,
+                    TutorialState: {
+                        TutorialStage: 0,
+                        TutorialControlID: 0,
                     },
-            },
-
-            Metal: new TieredAchievement(
-                [10,25,50,100,1000],
-                [1,1,2,2,5],
-                allEvents.EventTypes.METAL_RECIEVED,
-                function() {
-                    let base = Game.Persistents.Achievements.Metal;
-                        
-                    let nextTier = base.TierBreakpoints[base.BreakpointEarned];
-
-                    if (Game.Resources.Metal >= nextTier) {
-                        console.log(ParseGameText('Achievement Recieved: ' + GameText.English.AchievementText.Metal.Criteria, nextTier));
-
-                        Game.Persistents.Achievements.TotalScore += base.TierValues[base.BreakpointEarned++];
-                    }
-
-                    if (base.BreakpointEarned >= base.TierBreakpoints.length) {
-                        allEvents.removeEvent(base.HandlerID);
-                    }
                 }
-
-            )
-            
-        },
-
-        Stats: {
-            GameVersion: "NaNi",
-            LastUpdateTime: 0,
-            TutorialState: {
-                TutorialStage: 0,
-                TutorialControlID: 0,
             },
+
+            this.UIElements = {
+                ScrapCounter: document.querySelector('#scrapDisplay'),
+                MetalCounter: document.querySelector('#metalDisplay'),
+                LeatherCounter: document.querySelector('#leatherDisplay'),
+                ClothCounter: document.querySelector('#clothDisplay'),
+                MerylTurnOrder: document.querySelector('#MerylOrder'),
+                ChaseTurnOrder: document.querySelector('#ChaseOrder'),
+                TaliTurnOrder: document.querySelector('#TaliOrder'),
+                HerschelTurnOrder: document.querySelector('#HershelOrder'),
+                EnemyHealth: document.querySelector('#enemyHealth'),
+                WorldStats: document.querySelector('#WorldStats'),
+            },
+
+            // Settings
+            this.Settings = {
+                // Tick rate in MS
+                GameSpeed: 100,
+                AutoSaveFrequency: 30 * 60 * 1000, //30 minutes
+                NumberNotation: "Scientific",
+            }
         }
-    },
+    }
 
-    UIElements: {
-        ScrapCounter: document.querySelector('#scrapDisplay'),
-        MetalCounter: document.querySelector('#metalDisplay'),
-        LeatherCounter: document.querySelector('#leatherDisplay'),
-        ClothCounter: document.querySelector('#clothDisplay'),
-        MerylTurnOrder: document.querySelector('#MerylOrder'),
-        ChaseTurnOrder: document.querySelector('#ChaseOrder'),
-        TaliTurnOrder: document.querySelector('#TaliOrder'),
-        HerschelTurnOrder: document.querySelector('#HershelOrder'),
-        EnemyHealth: document.querySelector('#enemyHealth'),
-        WorldStats: document.querySelector('#WorldStats'),
-    },
-
-    // Settings
-    Settings: {
-        // Tick rate in MS
-        GameSpeed: 100,
-        AutoSaveFrequency: 30 * 60 * 1000, //30 minutes
-        NumberNotation: "Scientific",
-    },
-    
-}
-
-var GameResource = new GameClass();
+var Game = new GameStore();
